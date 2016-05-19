de53b557e1bd044328a708d798e1bddf9701b171
Nigel Gilbert <NGilbert@ethosdomain1.ethosgroup.com>
2016-05-19 15:14:53 -0500

    figured out json api controller

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using HelloDotnet;
 using Microsoft.AspNetCore.Hosting;
 
 public class Program
--- Startup.cs
+++ Startup.cs
@@ -3,50 +3,45 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using System.IO;
-using System;
 
-public class Startup
+namespace HelloDotnet
 {
-    // Called by the runtime. Use this method to add services to the container.
-    // Reference: https://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services)
+    public class Startup
     {
-        services.AddMvc();
-    }
+        // Reference: https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        // Called by the runtime. Use this method to add services to the container.
+        {
+            services.AddMvc();
+        }
 
-    // Called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app)
-    {
-        
-        app.UseMvc();
-        
-    }
-};
-//         // Serves index.html as the website route.
-//         app.UseDefaultFiles();
-        
-//         // Serve static files in wwwroot.
-//         app.UseStaticFiles();
-        
-//         // Serve static files from node_modules.
-//         string dir = Directory.GetCurrentDirectory();
-//         app.UseStaticFiles(new StaticFileOptions()
-//         {
-//             FileProvider = new PhysicalFileProvider(
-//                 Path.Combine(dir, "node_modules")
-//             ),
-//             RequestPath = new PathString("/node_modules")
-//         });
+        // Called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            // Serves index.html as the website route.
+            app.UseDefaultFiles();
+            
+            // Serve static files in wwwroot.
+            app.UseStaticFiles();
+            
+            // Serve static files from node_modules.
+            string dir = Directory.GetCurrentDirectory();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(dir, "node_modules")
+                ),
+                RequestPath = new PathString("/node_modules")
+            });
 
-//         // Configure the app's routing.
-    
-//         // app.UseMvc(config => 
-//         // {
-//         //     config.MapRoute(
-//         //         name: "Default",
-//         //         template: "{controller}/{action}/{id?}",
-//         //         defaults: new { controller = "App", action = "Index" }
-//         //     );    
-//         // });
-//     }
-// }
+            // Configure the app's routing.
+            app.UseMvc(config => 
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}"
+                );    
+            });
+        }
+    }
+}
